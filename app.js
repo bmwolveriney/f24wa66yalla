84f29349c445ae439298fe66274f80d16a5cc557
bmwolveriney <s573066@nwmissouri.edu>
2024-10-30 21:19:27 -0500

    new endpointtt

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var mydataRouter = require('./routes/mydata');
-
+var usersRouter = require('./routes/users'); // Add this line
+const mydataRouter = require('./routes/mydata');
 
 var app = express();
 
@@ -16,14 +15,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use('/mydata', mydataRouter);
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/mydata', mydataRouter);
+app.use('/users', usersRouter); // This will now work correctly
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
